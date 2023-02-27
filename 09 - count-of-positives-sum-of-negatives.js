8a75abd4152270ba5fd0f15e7151c2431e5064de
Connor Todd <connortodd0304@gmail.com>
2023-02-27 12:11:41 +0000

    Update 09 - count-of-positives-sum-of-negatives.js
    updated tests on algorithm 9

--- 09 - count-of-positives-sum-of-negatives.js
+++ 09 - count-of-positives-sum-of-negatives.js
@@ -13,19 +13,12 @@ function countPositivesSumNegatives(numbersArray) {
 // DO NOT TOUCH TESTS BELOW
 console.log(
     countPositivesSumNegatives(
-        [1, 2, 3, 4, -11, -12, -13, -14, -15]) === [4, -65] ?
+        [1, 2, 3, 4, 5, 6, 7, -11, -12])[0] === 7 ?
         '✅ 01 - Test Passed' : '❌ 01 - Test Failed'
 );
 
 console.log(
     countPositivesSumNegatives(
-        [1, 2, 3, 4, 5, 6, 7, -11, -12]) === [7, -23] ?
-        '✅ 01 - Test Passed' : '❌ 01 - Test Failed'
-);
-
-
-console.log(
-    countPositivesSumNegatives(
-        [3, 7, -18, -12]) === [7, -30] ?
+        [1, 2, 3, 4, 5, 6, 7, -11, -12])[1] === -23 ?
         '✅ 01 - Test Passed' : '❌ 01 - Test Failed'
 );
