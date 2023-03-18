3ad2ff9da6bd8f820ce4b516def9e6b36fc7fc63
Jay <jaygoos@live.nl>
2023-03-18 21:12:12 +0100

    solved fifth algo

--- 05 - sum-of-positives.js
+++ 05 - sum-of-positives.js
@@ -5,7 +5,13 @@
 
 
 function sumOfPositives(numbers) {
-    // Write your code here
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] >= 0) {
+            sum += numbers[i];
+        }
+    }
+    return sum;
 }
 
 
